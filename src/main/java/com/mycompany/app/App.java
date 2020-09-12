5d1bdc17a67522b073247582b700157e7554492b
sasikumarduraisamy84 <71162827+sasikumarduraisamy84@users.noreply.github.com>
2020-09-12 16:27:31 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,7 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
-        System.out.println(new App().getMessage() + "Jenkins");
+        //System.out.println(new App().getMessage() + "Jenkins");
     }
 
     private final String getMessage() {
